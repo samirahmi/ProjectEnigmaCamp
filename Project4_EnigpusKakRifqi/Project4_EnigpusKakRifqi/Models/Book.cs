7154f18854c58c747016ae90f3c51e2e34869b19
Rahmi Safitri <samirahmi28@gmail.com>
2022-11-14 03:58:23 +0700

    [FEATURE] Add Project4 Versi Kak Rifqi

---
+++ Project4_EnigpusKakRifqi/Project4_EnigpusKakRifqi/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4_EnigpusKakRifqi.Models
+{
+    public abstract class Book
+    {
+        public abstract string GetTitle();
+    }
+}
---
+++ Project4_EnigpusKakRifqi/Project4_EnigpusKakRifqi/Models/Magazine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4_EnigpusKakRifqi.Models
+{
+    public class Magazine : Book
+    {
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public int PublicationYear { get; set; }
+
+        public override string GetTitle()
+        {
+            return Title;
+        }
+        public override string ToString()
+        {
+            return $"Type: {nameof(Magazine)} {nameof(Code)}: {Code}, " +
+                $"{nameof(Title)}: {Title}, {nameof(Publisher)}: {Publisher}, " +
+                $"{nameof(PublicationYear)}: {PublicationYear} ";
+        }
+    }
+}
